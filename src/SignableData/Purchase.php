b6e7334a4906dd28b9faed661c090710c321554f
Muhannad Shelleh <muhannad.shelleh@gmail.com>
2017-06-08 18:35:08 +0400

    Init

---
+++ src/SignableData/Purchase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Muhannad Shelleh <muhannad.shelleh@live.com>
+ * Date: 6/8/17
+ * Time: 6:30 PM
+ */
+
+namespace ItvisionSy\Payfort\SignableData;
+
+class Purchase
+{
+
+}
---
+++ src/Signature.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Muhannad Shelleh <muhannad.shelleh@live.com>
+ * Date: 6/8/17
+ * Time: 6:18 PM
+ */
+
+namespace ItvisionSy\Payfort;
+
+class Signature
+{
+
+}
